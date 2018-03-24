0a22325723641960304f42a7197fe7897a3dbb8b
David Shepherd <davidshepherd7@gmail.com>
2018-03-24 11:46:19 +0000

    Make package-lint happy

--- fill-function-arguments.el
+++ fill-function-arguments.el
@@ -3,12 +3,35 @@
 ;; Copyright (C) 2015 Free Software Foundation, Inc.
 
 ;; Author: David Shepherd <davidshepherd7@gmail.com>
-;; Version: 0.3
+;; Version: 0.4
 ;; Package-Requires: ((emacs "24.5"))
-;; Keywords:
+;; Keywords: convenience
 ;; URL: https://github.com/davidshepherd7/fill-function-arguments
 
 ;;; Commentary:
+
+;; Add/remove line breaks between function arguments and similar constructs
+;;
+;; Put point inside the brackets and call `fill-function-arguments-dwim` to convert
+;;
+;; frobinate_foos(bar, baz, a_long_argument_just_for_fun, get_value(x, y))
+;;
+;; to
+;;
+;; frobinate_foos(
+;;                bar,
+;;                baz,
+;;                a_long_argument_just_for_fun,
+;;                get_value(x, y)
+;;                )
+;;
+;; and back.
+;;
+;; Also works with arrays (`[x, y, z]`) and dictionary literals (`{a: b, c: 1}`).
+;;
+;; If no function call is found `fill-function-arguments-dwim` will call `fill-paragraph`,
+;; so you can replace an existing `fill-paragraph` keybinding with it.
+;;
 
 
 ;;; Code:
